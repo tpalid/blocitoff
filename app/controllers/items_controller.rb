7772a895ed454e6b0095bc81437e703cd0c1139d
Theresa Palid <tpalid1@gmail.com>
2016-01-11 02:12:31 +0000

    Created partial for new items and added it to users view- user can now create new to do list item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,45 @@
 class ItemsController < ApplicationController
-  def index
+  # def index
     
-  end
+  # end
 
-  def show
-    @item = Item.find(params[:item_id])
-  end
+  # def show
+  #   @item = Item.find(params[:item_id])
+  # end
+  #The two above may not be necessary, unless i create a partial to display items on a user's page
 
   def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      flash[:notice] = "Item was saved."
+      redirect_to [current_user]
+      #will need to update this to use ajax
+    else
+      flash[:notice] = "There was an error saving the item.  Please try again."
+      #need to implement ajax on this
+      # render :new
+    end
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+  end
+  
+  def destroy
+    @item = Item.find(params[:id])
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:body)
   end
 end
