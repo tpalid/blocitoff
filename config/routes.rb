a16527005738e7b59c8e908c1092d47120177cf4
Theresa Palid <tpalid1@gmail.com>
2016-01-12 01:25:04 +0000

    Clean up- deleted unnecessary views and routes for items, nested item routes under users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :items
-
+  
   devise_for :users
   
   resources :users, only: [:show]
+    resources :items, only: [:create, :destroy, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
