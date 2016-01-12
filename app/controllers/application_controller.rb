e1e2823fc646bf47402e27c9f4c63344934a082e
Theresa Palid <tpalid1@gmail.com>
2016-01-12 16:25:44 +0000

    Edited Application Controller to allow users to sign up with and edit name through devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -6,4 +8,13 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(current_user)
   end
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :name, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
+  end
+  #Allows user to add and edit name, must be here because rails 4 has moved this to controller, so cannot be in user model
 end
