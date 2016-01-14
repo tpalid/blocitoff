7af41efe46a7db949c3bc58a283cb1fcf8a08578
Theresa Palid <tpalid1@gmail.com>
2016-01-14 17:18:31 +0000

    Styling edits

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ActiveRecord::Base
     belongs_to :user
     
+ # default_scope { order('DESC') }
+    
      def days_left
       lifespan.to_i - (DateTime.now.to_date - created_at.to_date).to_i
     end
