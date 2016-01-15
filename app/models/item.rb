eceb6b0d09c70a678799444857bc11bfc08e2260
Theresa Palid <tpalid1@gmail.com>
2016-01-15 18:34:54 +0000

    Catching up with remote

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ActiveRecord::Base
     belongs_to :user
     
- # default_scope { order('DESC') }
+    # default_scope { order('DESC') }
     
-     def days_left
+    def days_left
       lifespan.to_i - (DateTime.now.to_date - created_at.to_date).to_i
     end
 end
