ce0da66652d65c7bd4934786b943061cbb5f2916
Theresa Palid <tpalid1@gmail.com>
2016-01-21 19:53:16 +0000

    Lifespan progress, changed end date to datetime

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,15 @@
 class Item < ActiveRecord::Base
     belongs_to :user
     
-    # default_scope { order('DESC') }
+    default_scope { order('end_date DESC' :end_date) }
+    
+    def created_at
+      self.attributes[:created_at] ||= Time.now
+    end
+    
+     def lifespan
+        (self.end_date - self.created_at)
+    end
     
     def lifespan=(days)
         self.end_date = self.created_at + days.days
@@ -12,7 +20,7 @@ class Item < ActiveRecord::Base
     end
     
     def lifespan
-        self.end_date.to_days - self.created_at.to_days 
+        (self.end_date - self.created_at).to_f.to_days
     end
     
 end
