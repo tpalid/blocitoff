ad8fe660cd7b41b0b78c850fe8729053b3517481
Theresa Palid <tpalid1@gmail.com>
2016-01-30 17:36:52 +0000

    Fixed end date as nil bug

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,33 +3,21 @@ class Item < ActiveRecord::Base
     belongs_to :user
     require 'date'
     
-    # attr_writer :lifespan
-    # after_save :set_end_date
-    
-    # def set_end_date
-    #   if @lifespan
-    #     end_date = self.created_at + lifespan.days
-    #     self.update(end_date: end_date) unless self.end_date == end_date
-    #   end
-    # end
-    
     # default_scope { order('end_date DESC' :end_date) }
     
     def created_at
       created_at ||= Time.zone.now 
     end
     
-    # def end_date
-    #   end_date ||= Time.zone.now + 1.day
-    # end
     
     def lifespan=(days)
-        # self.end_date = self.created_at + days.days
         self.end_date = self.created_at + days.to_i.days
     end
     
     def lifespan
-      (self.end_date - self.created_at)/86400
+      if self.end_date.present?
+        (self.end_date - self.created_at)/1.day
+      end
     end
     
    
